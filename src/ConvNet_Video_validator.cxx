ec7991765ae7a4d1c04a161bfbda15bcbfc7dbce
Connor-Bowley <connor.bowley7@gmail.com>
2016-09-09 13:50:49 -0500

    Finished validator. Changes in CMakeLists_server

--- src/ConvNet_Video_validator.cxx
+++ src/ConvNet_Video_validator.cxx
@@ -12,6 +12,7 @@
 #include <sstream>
 #include <string>
 #include <vector>
+#include <algorithm>
 
 //from UNDVC_COMMON
 #include "parse_xml.hxx"
@@ -44,6 +45,7 @@ static inline std::string &trim(std::string &s)
 
 int init_result(RESULT& result, void*& data)
 {
+	//vector<OUTPUT_FILE_INFO> files;
 	OUTPUT_FILE_INFO fi;
 	try
 	{
@@ -62,19 +64,21 @@ int init_result(RESULT& result, void*& data)
 	}
 	catch(string error_message)
 	{
-		log_messages.printf(MSG_DEBUG,"ConvNet_Video_validation get_error_from_result([RESULT#%d %s]) failed with error: %s\n",result.id, result.name, error_message.c_str();
+		log_messages.printf(MSG_DEBUG,"ConvNet_Video_validation get_error_from_result([RESULT#%lu %s]) failed with error: %s\n",result.id, result.name, error_message.c_str());
 	}
 	catch(const exception &ex)
 	{
-		log_messages.printf(MSG_CRITICAL,"ConvNet_Video_validation get_error_from_result([RESULT#%d %s]) failed with error: %s\n",result.id, result.name, ex.what());
+		log_messages.printf(MSG_CRITICAL,"ConvNet_Video_validation get_error_from_result([RESULT#%lu %s]) failed with error: %s\n",result.id, result.name, ex.what());
 		exit(0);
-		return retval;
+		return 1;
 	}
 
 	int retval = get_output_file_path(result, fi.path);
+	//int retval = get_output_file_infos(result, files);
+	//OUTPUT_FILE_INFO &fi = files[0];
 	if(retval)
 	{
-		log_messages.printf(MSG_CRITICAL, "ConvNet_Video_validation: Failed to get output file path: %d %s\n",result.id, result.name);
+		log_messages.printf(MSG_CRITICAL, "ConvNet_Video_validation: Failed to get output file path: %lu %s\n",result.id, result.name);
 		exit(0);
 		return retval;
 	}
@@ -94,12 +98,12 @@ int init_result(RESULT& result, void*& data)
 		res->names.push_back(itemtemp);
 	
 	//then get all the numbers from the rest of the rows.
-	while(getline(infile,linetemp)
+	while(getline(infile,linetemp))
 	{
 		//now linetemp holds a comma separated line
 		res->outputNums.resize(res->outputNums.size() + 1);
 		ss.str(linetemp);
-		while(getline(linetemp,itemtemp,','))
+		while(getline(ss,itemtemp,','))
 			res->outputNums.back().push_back(stod(itemtemp));
 	}
 	infile.close();
@@ -112,7 +116,7 @@ int init_result(RESULT& result, void*& data)
 
 int compare_results(RESULT &r1, void *data1, RESULT &r2, void *data2, bool& match)
 {
-	float threshodl = 0.015;
+	float threshold = 0.015;
 	cnn_output *res1 = (cnn_output*)data1;
 	cnn_output *res2 = (cnn_output*)data2;
 
@@ -124,6 +128,39 @@ int compare_results(RESULT &r1, void *data1, RESULT &r2, void *data2, bool& matc
 		return 0;
 	}
 
-	log_messages.printf(MSG_DEBUG, "Check number of frames ran.\n");
-	
+	log_messages.printf(MSG_DEBUG, "Check number of frames run.\n");
+	if(res1->outputNums.size() != res2->outputNums.size())
+	{
+		match = false;
+		log_messages.printf(MSG_CRITICAL, "ERROR, number of frames run is different. %lu vs %lu\n", res1->outputNums.size(), res2->outputNums.size());
+		return 0;
+	}
+	//check all numbers for match within threshold
+	for(unsigned int i = 0; i < res1->outputNums.size();i++)
+	{
+		unsigned int endj = res1->outputNums[i].size();
+		for(unsigned int j = 0; j < endj; j++)
+		{
+			double val1 = res1->outputNums[i][j];
+			double val2 = res2->outputNums[i][j];
+
+			double diff = abs(val1 - val2);
+			if(diff > threshold)
+			{
+				match = false;
+				log_messages.printf(MSG_CRITICAL, "ERROR, calculated values differ by more than threshold. %lf vs %lf\n",val1, val2);
+				return 0;
+			}
+		}
+	}
+	match = true;
+	log_messages.printf(MSG_DEBUG,"Everything seems to match.\n");
+	return 0;
+}
+
+int cleanup_result(RESULT const&, void *data)
+{
+	cnn_output* res = (cnn_output*)data;
+	delete res;
+	return 0;
 }
